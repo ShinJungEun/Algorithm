dd06edc90173b62ea5e39a514f84d407f4a637ee
JUNGEUN <sjy8033@naver.com>
2020-03-08 15:05:23 +0900

    상근날드

---
+++ src/problem/p5543.java
@@ -0,0 +1,36 @@
+package problem;
+
+import java.util.Scanner;
+
+public class p5543 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int arr[] = new int[5];
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = input.nextInt();
+		}
+		
+		int min = 2001;
+		int sum = 0;
+		for(int i = 0; i < 3; i++) {
+			if(arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		sum += min;
+		min = 2001;
+		for(int i = 3; i < arr.length; i++) {
+			if(arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		sum += min;
+		sum -= 50;
+		
+		System.out.println(sum);
+		
+	}
+
+}
