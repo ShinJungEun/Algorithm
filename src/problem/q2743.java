d0fad38aee20ac01849d2a619c1d1c85268c2394
JUNGEUN <sjy8033@naver.com>
2020-03-10 23:22:56 +0900

    단어 길이 재기

---
+++ src/problem/q2743.java
@@ -0,0 +1,15 @@
+package problem;
+
+import java.util.Scanner;
+
+public class q2743 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String str = input.next();
+		
+		System.out.println(str.split("").length);
+	}
+
+}
