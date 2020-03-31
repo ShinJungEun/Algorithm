94753b5fd35dbf515cfbf4c190b3c5559094a64d
정은 <38749778+ShinJungEun@users.noreply.github.com>
2020-03-31 09:38:08 +0900

    합분해 (DP) ★

---
+++ src/problem/q2225.java
@@ -0,0 +1,31 @@
+package problem;
+
+import java.util.Scanner;
+
+public class q2225 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		int k = input.nextInt();
+		
+		// dp[k][n]
+		int dp[][] = new int[201][201];
+		for(int i = 0; i <= n; i++) {
+			dp[1][i] = 1;
+		}
+		
+		for(int i = 1; i <= k; i++) {
+			for(int j = 0; j <= n; j++) {
+				for(int m = 0; m <= j; m++) {
+					dp[i][j] += dp[i - 1][j - m];
+					dp[i][j] %= 1000000000;
+				}
+			}
+		}
+		
+		System.out.println(dp[k][n]);
+	}
+
+}
