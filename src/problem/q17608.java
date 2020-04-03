0f5136e0db7d1375eb76261005cc19b369e620b3
정은 <38749778+ShinJungEun@users.noreply.github.com>
2020-04-03 10:15:31 +0900

    막대기

---
+++ src/problem/q17608.java
@@ -0,0 +1,28 @@
+package problem;
+
+import java.util.Scanner;
+
+public class q17608 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		int arr[] = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = input.nextInt();
+		}
+		
+		int count = 0;
+		int max = 0;
+		for(int i = n - 1; i >= 0 ; i--) {
+			if(arr[i] > max) {
+				max = arr[i];
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+	}
+
+}
