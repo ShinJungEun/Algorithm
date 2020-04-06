60d556ec1d7846791f7b165306b352b9563cfb2c
정은 <38749778+ShinJungEun@users.noreply.github.com>
2020-04-06 10:26:23 +0900

    막대기

---
+++ src/problem/q1094.java
@@ -0,0 +1,22 @@
+package problem;
+
+import java.util.Scanner;
+
+public class q1094 {
+
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      
+      int n = input.nextInt();
+      
+      int count = 0;
+      while(n > 0) {
+         if(n % 2 == 1) {
+            count++;
+         }
+         n /= 2;
+      }
+      System.out.println(count);
+   }
+
+}
