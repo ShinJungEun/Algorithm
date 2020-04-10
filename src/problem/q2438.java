ba56f93d0bb5d14decd1a0bab1d9a9e20dce340e
정은 <38749778+ShinJungEun@users.noreply.github.com>
2020-04-10 23:55:19 +0900

    별찍기 - 1

---
+++ src/problem/q2438.java
@@ -0,0 +1,20 @@
+package problem;
+
+import java.util.Scanner;
+
+public class q2438 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		
+		String s = "";
+		for(int i = 0; i < n; i++) {
+			s += "*";
+			System.out.println(s);
+		}
+			
+	}
+
+}
