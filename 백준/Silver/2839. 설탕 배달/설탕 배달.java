0366c78b0a6bb307ced46e2afe4869193da1e668
정은 <38749778+ShinJungEun@users.noreply.github.com>
2022-09-28 22:32:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int count = 0;
+		
+		while(n > 0) {
+			if(n % 5 == 0) {
+				count += (n / 5);
+				n = 0;
+			} else {
+				if(n < 3) {
+					count = -1;
+					break;
+				}
+				n -= 3;
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+}
