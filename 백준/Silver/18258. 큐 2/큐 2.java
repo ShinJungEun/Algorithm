27c7b0871d661f347f8f8eaaae2eb0e1e9c5ec22
정은 <38749778+ShinJungEun@users.noreply.github.com>
2022-11-25 18:19:30 +0900

    [Silver IV] Title: 큐 2, Time: 1320 ms, Memory: 474332 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+//		push X: 정수 X를 큐에 넣는 연산이다.
+//		pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//		size: 큐에 들어있는 정수의 개수를 출력한다.
+//		empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
+//		front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//		back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		LinkedList<String> q = new LinkedList<String>();
+		for(int i = 0; i < n; i++) {
+			String qStr = br.readLine();
+			String[] qArr = qStr.split(" ");
+			switch(qArr[0]) {
+			case "push":
+				q.add(qArr[1]);
+				break;
+			case "pop":
+				bw.write((q.size() > 0 ? q.poll() : "-1") + "\n");
+				break;
+			case "size":
+				bw.write(q.size() + "\n");
+				break;
+			case "empty":
+				bw.write((q.isEmpty() ? "1" : "0") + "\n");
+				break;
+			case "front":
+				bw.write((q.size() > 0 ? q.peekFirst() : "-1") + "\n");
+				break;
+			case "back":
+				bw.write((q.size() > 0 ? q.peekLast() : "-1") + "\n");
+				break;
+			}
+		}
+		bw.flush();
+        bw.close();
+	}
+
+}
