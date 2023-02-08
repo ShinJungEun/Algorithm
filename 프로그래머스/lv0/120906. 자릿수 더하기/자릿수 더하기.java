b357e5a353b09e389a82e6ccff097fc1534002e6
정은 <38749778+ShinJungEun@users.noreply.github.com>
2023-02-08 18:00:49 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = n;
+        while(num > 0) {
+            answer += num%10;
+            num /= 10;
+        }
+        return answer;
+    }
+}
