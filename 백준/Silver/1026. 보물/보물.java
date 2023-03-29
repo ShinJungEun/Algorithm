2d8210780db4ff6811b94387e4f778bd72d83b10
정은 <38749778+ShinJungEun@users.noreply.github.com>
2023-03-29 17:26:29 +0900

    [Silver IV] Title: 보물, Time: 112 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int n = input.nextInt();
+		int arr1[] = new int[n];
+		int arr2[] = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr1[i] = input.nextInt();
+		}
+		for(int i = 0; i < n; i++) {
+			arr2[i] = input.nextInt();
+		}
+		aSort(arr1);
+		dSort(arr2);
+		
+		int minSum = 0;
+		for(int i = 0; i < n; i++) {
+			minSum += (arr1[i] * arr2[i]);
+		}
+		System.out.println(minSum);
+	}
+
+	// 오름차순 정렬
+	public static void aSort(int arr[]) {
+		for(int i = 1; i < arr.length; i++) {
+			int key = arr[i];
+			int j = i - 1;
+			while((j >= 0) && (key < arr[j])) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
+	}
+
+	// 내림차순 정렬
+	public static void dSort(int arr[]) {
+		for(int i = 1; i < arr.length; i++) {
+			int key = arr[i];
+			int j = i - 1;
+			while((j >= 0) && (key > arr[j])) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
+	}
+}
