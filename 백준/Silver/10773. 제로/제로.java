1efc99a2fc3671c4870c38ab0396a6a6cb6fc157
정은 <38749778+ShinJungEun@users.noreply.github.com>
2023-03-29 17:46:54 +0900

    [Silver IV] Title: 제로, Time: 692 ms, Memory: 94952 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Stack<Integer> st = new Stack<>();
+		
+		int k = input.nextInt();
+
+		int n = 0;
+		for(int i = 0; i < k; i++) {
+			n = input.nextInt();
+			if(n == 0) {
+				st.pop();
+			}
+			else {
+				st.push(n);
+			}
+		}
+		
+		int sum = 0;
+		while(!st.isEmpty()) {
+			sum += st.pop();
+		}
+		System.out.println(sum);
+	}
+
+}
