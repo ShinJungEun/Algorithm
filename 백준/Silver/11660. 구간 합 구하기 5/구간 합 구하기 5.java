aca4a67e93421b08406a7885044272e4d559ac1d
정은 <38749778+ShinJungEun@users.noreply.github.com>
2025-07-02 16:16:06 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 804 ms, Memory: 124772 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[n+1][n+1]; // 입력 배열
+		int [][]sumArr = new int[n+1][n+1]; // 누적합 배열
+		
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1; j <= n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		// 배열 누적합 구하기
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				sumArr[i][j] = sumArr[i - 1][j] + sumArr[i][j - 1]
+                        - sumArr[i - 1][j - 1] + arr[i][j];
+			}
+		}
+		
+		// 연산
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int sum = sumArr[x2][y2] - sumArr[x1-1][y2] - sumArr[x2][y1-1] + sumArr[x1-1][y1-1];
+			bw.write(sum + "\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
